75231d3571cf63a457b9f57e697f9b9696386ac7
TGVaporeon (Doremy Sweet) <jirachi7593@msn.com>
2016-07-30 16:22:20 -0500

    Added Proper Grammar

--- bot.js
+++ bot.js
@@ -5,13 +5,13 @@ const config = JSON.parse((require("fs")).readFileSync("./config.json"));
 const client = new Eris.CommandClient(config.token, {} , {
     prefix: ["[]", "@mention"],
     owner: "acdenisSK, TGVaporeon",
-    description: "just a bot to share the link to the real discord sidescroller, might have something more in the future though"
+    description: "Just a bot to share the link to the real discord sidescroller, might have something more in the future though!"
 });
 
 client.once("ready", () => console.log("Running!"));
 
-client.registerCommand("play", "here's the link to it!\nhttps://acdenissk.github.io/discordsidescroller/index.html");
+client.registerCommand("play", "Here's the link to where you can play!\nhttps://acdenissk.github.io/discordsidescroller/index.html");
 
-client.registerCommand("server", "here's the link to my server!\nhttps://discord.gg/sfBskxg");
+client.registerCommand("server", "Here's the link to my server!\nhttps://discord.gg/sfBskxg");
 
 client.connect();
